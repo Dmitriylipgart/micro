1cf35db0c64cd1a6dd7b0d245c652e8599788d3d
dmitriy.lipgart <dmitriylipgart@gmail.com>
2019-09-26 18:12:47 +0300

    added auth config

--- zuul-gateway/src/main/java/com/example/zuulgateway/config/GatewayConfig.java
+++ zuul-gateway/src/main/java/com/example/zuulgateway/config/GatewayConfig.java
@@ -1,6 +1,4 @@
-package com.example.auth.config;
-
-import com.example.auth.service.UserDetailService;
+package com.example.zuulgateway.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
